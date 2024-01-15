6ce074c74d8253860fd32999c4ad17a39eee7ec4
grossiValentina <grossivalentina435@gmail.com>
2024-01-15 16:05:03 +0100

    add:compilazione delle card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Movies</h1>
+<h1 class="text-center fw-bold fst-italic p-4">Movies</h1>
 <div class="container">
-    <div class="row row-cols-3">
-        <div class="col">
+    <div class="row d-flex justify-content-between">
+        
             <!-- card -->
             @foreach ($movies as $movie)
-            <div class="card" style="width: 15rem;">
-                <img src=" {{ $movie->image }} " class="card-img-top" alt="...">
+            <div class="card bg-light mb-5" style="width: 15rem;">
+                <img src=" {{ $movie->image }} " class="card-img-top w-100" alt="...">
                 <h5 class="card-body text-center p-2 m-0">
                     {{ $movie->title }}
                 </h5>
-                <p class="text-center fw-lighter fst-italic">Original Title: {{ $movie->original_title }}</p>
+                <p class="text-center fw-lighter fst-italic mb-0">Original Title: {{ $movie->original_title }}</p>
+                <p class="text-center fw-lighter fst-italic">Nationality: {{ $movie->nationality }}</p>
             </div>
             @endforeach
-        </div>
+    
     </div>
 
 </div>
